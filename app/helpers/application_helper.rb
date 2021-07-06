70e4462b774f74a31c8567c913787773809e7aa8
Andy Kallhoff <andykallhoff@sixgen.io>
2021-07-06 07:40:44 -0600

    adds lead pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def display_phone(number)
+    number = number.delete("^0-9")
+    # for numbers like '1-800-123-1234'
+    if number.length == 11 && number[0] == '1'
+      return number_to_phone(number[1..-1], pattern: /(\d{3})(\d{3})(\d{4})$/, area_code: true, delimiter: '-')
+    end
+
+    # for numbers like '800-123-1234'
+    return number_to_phone(number, pattern: /(\d{3})(\d{3})(\d{4})$/, area_code: true, delimiter: '-') if number.length == 10
+
+    # for foreign numbers
+    number_to_phone(number, pattern: /(\d{1,4})(\d{4})(\d{4})$/, delimiter: '-')
+  end
 end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,4 +2,8 @@ class Lead < ApplicationRecord
   validates :first_name, :last_name, :gender, :age, :email, :phone_number, presence: true
 
   enum gender: { male: 0, female: 1, other: 2 }
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
